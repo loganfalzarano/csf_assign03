2c970cefe49505e206cc7060cc1abc0d888fe960
loganfalzarano <lfalzar1@jhu.edu>
2022-03-10 21:17:03 -0500

    yay

--- csim.cpp
+++ csim.cpp
@@ -10,7 +10,10 @@ using namespace std;
 //TODO: should we name this a Block
 class Slot {
     string tag;
+    bool is_valid;
     bool dirty_bit;
+    unsigned access_ts;
+    unsigned load_ts;
 
     public:
         //constructor for creating a slot
@@ -77,18 +80,17 @@ class Cache {
         }          
 };
 
-//TODO: Could also do with bit shifting
 int is_power_of_two(int n) {
-    // if (n == 0) {
-    //     return 0;
-    // }
-    // while (n != 1) {
-    //     n /= 2;
-    //     if (n%2 != 0 && n != 1) {
-    //         return 0;
-    //     }
-    // }
-    // return 1; //if we get out of the loop return 1
+    /* if (n == 0) {
+        return 0;
+    }
+    while (n != 1) {
+        n /= 2;
+        if (n%2 != 0 && n != 1) {
+            return 0;
+        }
+    }
+    return 1; //if we get out of the loop return 1 */
     return (n != 0) && ((n & (n - 1)) == 0); // pretty sure this works but same thing either way
 }
 
@@ -98,14 +100,16 @@ bool check_command_line_args(int sets_in_cache, int blocks_in_set, int bytes_in_
         cout << "Too many or too few command line arguments (there must be exactly 7).\n"; // should print to stderr i think
         return false;
     }
-    // if (allocate_type.compare("no-write-allocate") && write_type.compare("write-back")) {
-    //     std::cerr << "combine no-write-allocate with write-back.\n";
-    //     return false;
-    // }
-    if (bytes_in_block < 4) {
-        std::cerr << "cannot have block size is less than 4.\n";
+
+    //old cold
+    /* if (allocate_type.compare("no-write-allocate") && write_type.compare("write-back")) {
+        std::cerr << "combine no-write-allocate with write-back.\n";
         return false;
     }
+    if (bytes_in_block < 4) {
+        cerr << "cannot have block size is less than 4.\n";
+        return false;
+    } */
 
     sets_in_cache = atoi(argv[1]); //atoi could fail when we have 1234bad_input 
     blocks_in_set = atoi(argv[2]);
@@ -114,14 +118,15 @@ bool check_command_line_args(int sets_in_cache, int blocks_in_set, int bytes_in_
     write_type = argv[5];
     eviction_type = argv[6];
 
-    bool c1 = is_power_of_two(sets_in_cache); //atoi could fail when we have 1234bad_input 
+    bool c1 = is_power_of_two(sets_in_cache);
     bool c2 = is_power_of_two(blocks_in_set);
     bool c3 = is_power_of_two(bytes_in_block) && bytes_in_block >= 4;
     bool c4 = allocate_type.compare("write-allocate")==0 || allocate_type.compare("no-write-allocate")==0;
     bool c5 = (write_type.compare("write-through")==0 || write_type.compare("write-back")==0);
     bool c6 = (eviction_type.compare("lru")==0 || eviction_type.compare("fifo")==0);
+    bool c7 = !(allocate_type.compare("no-write-allocate") && write_type.compare("write-back"));
 
-    if (c1 && c2 && c3 && c4 && c5 && c6) {
+    if (c1 && c2 && c3 && c4 && c5 && c6 & c7) {
         return true;
     }
     //TODO: give user more feedback on how to give command line args
@@ -130,12 +135,7 @@ bool check_command_line_args(int sets_in_cache, int blocks_in_set, int bytes_in_
 
 }
 
-//probably good to make this member function inside the Cache class eventually
-
-
 int main(int argc, char** argv) {
-    
-    //TODO: Check if there are a correct number of inputs
 
     int sets_in_cache, blocks_in_set, bytes_in_block;
     string allocate_type, write_type, eviction_type;
