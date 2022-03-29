53c451acff480d59a6df66b73202e9c7611de76f
Tal Magdish <talmagdish@gmail.com>
2022-03-29 18:44:07 -0400

    deleted comments

--- csim.cpp
+++ csim.cpp
@@ -7,7 +7,6 @@
 
 using namespace std;
 
-//TODO: should we name this a Block
 class Slot {
     public:
         u_int32_t tag; //we want this to be a binary value
@@ -33,7 +32,6 @@ class Set {
 
 class Cache {
     vector<Set> cache;
-    //cache parameters (could declare in global scope)
     u_int32_t sets_in_cache;
     u_int32_t blocks_in_set;
     u_int32_t bytes_in_block;
@@ -58,7 +56,7 @@ class Cache {
     
     public:
         Cache(u_int32_t sets_in_cache, u_int32_t blocks_in_set, u_int32_t bytes_in_block, bool write_allocate, bool no_write_allocate, bool write_through, bool write_back, bool FIFO, bool LRU) {
-            //construct the cache parameters (WE MIGHT NOT NEED THESE HERE)
+            //construct the cache parameters
             this->sets_in_cache = sets_in_cache;
             this->blocks_in_set = blocks_in_set;
             this->bytes_in_block = bytes_in_block;
@@ -73,18 +71,13 @@ class Cache {
             offset_bits = bitshift_log_base2(bytes_in_block);
             index_bits = bitshift_log_base2(sets_in_cache);
             tag_bits = 32 - index_bits - offset_bits;
-            // cout << "Offset bits:" << offset_bits << endl;
-            // cout << "Index bits:" << index_bits << endl;
-            // cout << "Tag bits:" << tag_bits << endl;
 
-            //TODO: Instantiate vector<Set>
             for(u_int32_t i = 0; i < sets_in_cache; i++) {
                 Set new_set;
                 cache.push_back(new_set);
             }
         }
 
-        //TODO: Check if bitwise logbase2 is correct
         int bitshift_log_base2(int n) {
             int res = 0;
             while (n != 1 && n != 0) {
@@ -96,7 +89,6 @@ class Cache {
 
         //method to give a trace on a give input file
         void begin_trace() {
-            //TODO determine if we need to deal with invalid trace files
             string input_line;
             while(getline(cin, input_line)) {
                 string read_or_write = input_line.substr(0, 1);
@@ -104,29 +96,18 @@ class Cache {
                 u_int32_t memory_address = stoul(string_memory_address, 0, 16);
                 
                 u_int32_t tag = memory_address >> (32 - tag_bits);
-                //TODO: Make this more simple
                 u_int32_t index = (memory_address & (((1UL << index_bits) - 1) << offset_bits)) >> offset_bits;
 
-                // cout << "tag is: " << tag << endl;
-                // cout << "index is: " << index << endl;
-                // cout << input_line << endl;
-                // cout << endl;
-
                 if(read_or_write.compare("l") == 0) {
-                    //cout << "about to load a value" << endl;
                     load_value(index, tag);
-                    //cout << "returned";
                 } else if (read_or_write.compare("s") == 0) {
-                    //cout << "about to load a value" << endl;
                     store_value(index, tag);
                 }
-                //cout << "____________"<< endl;
             }
         }
 
         //returns index of the hit in the set if we can find it and -1 otherwise
         int find(const Set & set, u_int32_t tag) {
-            //cout << "searching for a hit" << set.slots.size() << endl;
             for (u_int32_t i=0; i< set.slots.size(); i++) {
                 if (set.slots[i].tag == tag) {
                     return i; //we found the given tag
@@ -137,12 +118,9 @@ class Cache {
 
         int find_index_to_evict(const Set & set) {
             int index_to_evict = 0;
-            //cout << "entered eviction function" << endl;
             for (u_int32_t i=0; i<set.slots.size(); i++) {
                 
                 if (LRU == true) { // for lru find the least recently accessed
-                    //cout << "got here" << endl;
-                    //cout << set.slots[i].access_ts << " < " << set.slots[index_to_evict].access_ts << endl;
                     if (set.slots[i].access_ts < set.slots[index_to_evict].access_ts) {
                         index_to_evict = i;
                     }
@@ -152,7 +130,6 @@ class Cache {
                     }
                 }
             }
-            //cout << "INdec to evict is:" << index_to_evict << endl;
             return index_to_evict;
         }
 
@@ -164,27 +141,21 @@ class Cache {
                 if ((write_back == true) && cache[index].slots[index_to_evict].is_dirty) { 
                     total_cycles += 100 * bytes_in_block / 4;
                 }
-                //cout << "Evicting tag:" << cache[index].slots[index_to_evict].tag << " index " << index;
                 cache[index].slots[index_to_evict] = new_slot;
             } else { //no eviction needed, add it to the set
-                //cout << "About to place a slot at the end" << endl;
                 cache[index].slots.push_back(new_slot);
-                //cout << cache[index].slots.size() << endl;
             }
             total_cycles++; //adding to a set in the cache takes one cycle
         }
 
         void load_value(u_int32_t index, u_int32_t tag) {
             total_loads++;
-            //cout << "|" << total_loads << "|" << endl;
             Set set_accessed = cache[index];
             int hit = find(set_accessed, tag);
             if (hit != -1) { //load hit
-                //cout << "it was a load hit" << endl;
                 cache[index].slots[hit].access_ts = total_cycles; //update access ts
                 load_hits++;
                 total_cycles++;
-                //cout << "it was a load miss" << endl;
                 
             } else { //load miss
                 load_misses++;
@@ -192,52 +163,10 @@ class Cache {
                 Slot new_slot = Slot(tag, false, total_cycles, total_cycles); //slot is not different from memory so dirty_bit is false
                 //if have a miss we need to add the new slot to the set in the cache
                 add_to_set(index, new_slot);
-                //cout << cache[index].slots.size() << "<- Number of things in slot" << endl;
                 //load from memory since we missed
-                
             }
 
         }
-
-        /* void store_value(u_int32_t index, u_int32_t tag) {
-            total_stores++;
-            Set set_accessed = cache[index];
-            int hit = find(set_accessed, tag);
-            //store miss
-
-            if (hit == -1) {
-                cout << "STORE MISS" << endl;
-                //if we miss we need to get the block from memory according to the allocate policy
-                store_misses++;
-                if (allocate_type.compare("no-write-allocate") == 0) {
-                    total_cycles += 100; //write straight to memory, no write to cache
-                } else if (allocate_type.compare("write-allocate") == 0) {
-                    Slot new_slot = Slot(tag, true, total_cycles, total_cycles);
-                    add_to_set(index, new_slot);
-                    total_cycles += (100 * (bytes_in_block / 4)); //load the block from main into the cache
-                }
-            } else {
-                cout << "STORE HIT" << endl;
-                store_hits++;
-            }
-            // once we have loaded the block accordingly, then we proceed with writing
-
-            // in write-through mode, we write the` block straight to main memory, regardless of whether we just loaded it into the cache
-            if (write_type.compare("write-through") == 0) {
-                if (hit != -1) {
-                    cache[index].slots[hit].access_ts = total_cycles; //don't think we need this
-                }
-                total_cycles += 100;
-            //in write-back mode
-            } else if (write_type.compare("write-back") == 0) {
-                //write to Cache and mark it as dirty
-                Slot new_slot = Slot(tag, true, total_cycles, total_cycles); //these two lines feel repeated from within write-allocate
-                add_to_set(index, new_slot); //I think we might be able to get rid of them because write allocate takes care of writing to the cache
-                total_cycles++;
-                //cache[index].slots.push_back(new_slot);
-                //add_to_set(index, new_slot);
-            }
-        } */
 
         void store_value(u_int32_t index, u_int32_t tag) {
             total_stores++;
@@ -253,8 +182,6 @@ class Cache {
                     cache[index].slots[hit].is_dirty = true;
                     cache[index].slots[hit].access_ts = total_cycles; //can take out of if
                     //no eviciotn needed since we hit
-                    // Slot new_slot = Slot(tag, true, total_cycles, total_cycles); //slot is not different from memory so dirty_bit is false
-                    // add_to_set(index, new_slot);
                 }
                 total_cycles++; //write to cache takes 1 cycle
             } else {
@@ -293,17 +220,7 @@ class Cache {
 };
 
 int is_power_of_two(int n) {
-    /* if (n == 0) {
-        return 0;
-    }
-    while (n != 1) {
-        n /= 2;
-        if (n%2 != 0 && n != 1) {
-            return 0;
-        }
-    }
-    return 1; //if we get out of the loop return 1 */
-    return (n != 0) && ((n & (n - 1)) == 0); // pretty sure this works but same thing either way
+    return (n != 0) && ((n & (n - 1)) == 0);
 }
 
 //returns true if there are enough command line arguments and they are all valid
@@ -313,17 +230,7 @@ bool check_command_line_args(u_int32_t* sets_in_cache, u_int32_t* blocks_in_set,
         return false;
     }
 
-    //old code
-    /* if (allocate_type.compare("no-write-allocate") && write_type.compare("write-back")) {
-        std::cerr << "combine no-write-allocate with write-back.\n";
-        return false;
-    }
-    if (bytes_in_block < 4) {
-        cerr << "cannot have block size is less than 4.\n";
-        return false;
-    } */
-
-    * sets_in_cache = atoi(argv[1]); //atoi could fail when we have 1234bad_input 
+    * sets_in_cache = atoi(argv[1]);
     * blocks_in_set = atoi(argv[2]);
     * bytes_in_block = atoi(argv[3]);
     * allocate_type = argv[4];
@@ -342,7 +249,6 @@ bool check_command_line_args(u_int32_t* sets_in_cache, u_int32_t* blocks_in_set,
         return true;
     }
 
-    //TODO: give user more feedback on how to give command line args
     std::cerr << "Invalid command line arguments.\n";
     return false;
 
